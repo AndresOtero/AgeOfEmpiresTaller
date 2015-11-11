5a3c97b4e7a4e45e8579bab1ed6da044c0decf67
AndresOtero <oteroandres95@gmail.com>
2015-11-11 16:11:36 -0300

    Crear Edificio

---
+++ AgeOfEmpiresTaller/ModeloSrc/FactoryEdificio.cpp
@@ -0,0 +1,18 @@
+/*
+ * FactoryEdificio.cpp
+ *
+ *  Created on: 11 de nov. de 2015
+ *      Author: andres
+ */
+
+#include "FactoryEdificio.h"
+
+FactoryEdificio::FactoryEdificio() {
+	// TODO Auto-generated constructor stub
+
+}
+
+FactoryEdificio::~FactoryEdificio() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ AgeOfEmpiresTaller/ModeloSrc/FactoryEdificio.h
@@ -0,0 +1,17 @@
+/*
+ * FactoryEdificio.h
+ *
+ *  Created on: 11 de nov. de 2015
+ *      Author: andres
+ */
+
+#ifndef FACTORYEDIFICIO_H_
+#define FACTORYEDIFICIO_H_
+
+class FactoryEdificio {
+public:
+	FactoryEdificio();
+	virtual ~FactoryEdificio();
+};
+
+#endif /* FACTORYEDIFICIO_H_ */
