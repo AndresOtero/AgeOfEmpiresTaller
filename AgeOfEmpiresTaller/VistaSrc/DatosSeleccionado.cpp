c83474f9d28c05826fa8be17862b7866bd4c1085
tobiasbianchi <tobias.bianchi93@gmail.com>
2015-11-14 14:30:20 -0300

    mirar commit anterior

---
+++ AgeOfEmpiresTaller/VistaSrc/DatosSeleccionado.cpp
@@ -0,0 +1,26 @@
+/*
+ * DatosSeleccionado.cpp
+ *
+ *  Created on: Nov 14, 2015
+ *      Author: tobias
+ */
+
+#include "DatosSeleccionado.h"
+
+DatosSeleccionado::DatosSeleccionado() {
+	this->vida_total=0;
+	this->vida_actual=0;
+	this->tipo=VACIO;
+	this->recurso=0;
+	this->nombre="";
+	this->jugador="";
+	this->consturccion_actual=0;
+	this->construccion_total=0;
+	// TODO Auto-generated constructor stub
+
+}
+
+DatosSeleccionado::~DatosSeleccionado() {
+	// TODO Auto-generated destructor stub
+}
+
